cf0104986de9a241f254de3242913c2d110cc516
MauricioPB00 <mauricioperinbecker@gmail.com>
2023-08-17 14:47:17 -0300

    estoque-css

--- src/app/estoque/estoque.component.ts
+++ src/app/estoque/estoque.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-estoque',
   templateUrl: './estoque.component.html',
   styleUrls: ['./estoque.component.css']
 })
-export class EstoqueComponent {
+export class EstoqueComponent implements OnInit {
+  modalVisible: boolean = false;
 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  abrirModal( ) {
+    console.log('1111');
+    this.modalVisible = true;
+  }
+
+  fecharModal() {
+    console.log('2222')
+    this.modalVisible = false;
+  }
+
+  onSubmit() {
+    console.log('111');
+    this.abrirModal();
+  }
 }
