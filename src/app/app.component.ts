797512b905fc780e1de699a5b53c544c246cddcf
MauricioPB00 <mauricioperinbecker@gmail.com>
2023-09-13 22:26:42 -0300

    f4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {
+  }
+
   title = 'routing-perin';
+
+  ngOnInit() {
+    this.router.events
+
+      .subscribe((routerevent) => {
+
+        if (routerevent instanceof NavigationEnd) {
+          window.addEventListener('keydown', (event) => {
+            if (event.key === 'F4') {
+              event.preventDefault();
+              console.log('Tecla F4 ');
+              if (routerevent.url == '/condi') {
+                window.open('https://www.google.com/', '_blank');
+              }
+              if (routerevent.url == '/home') {
+                window.open('https://www.facebook.com/', '_blank');
+              }
+            }
+          });
+        }
+      });
+  }
 }
